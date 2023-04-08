ed4ad3ac58a47ca19f82831d1acd609c84d5ee26
AditiAditiCU <editi@192.168.2.67>
2023-04-08 15:56:48 -0400

    Pushing Changes For GetCause Detector

---
+++ Assignment/Antipattern_Detector/src/main/java/com/concordia/soen/sma/GetCauseDetector.java
@@ -0,0 +1,53 @@
+package com.concordia.soen.sma;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+
+class GetCauseDetector extends ASTVisitor {
+	
+
+	private CompilationUnit cu;
+	private String filePath;
+	private Writer writer;
+
+	public GetCauseDetector(CompilationUnit cu, String filePath, Writer writer) {
+		this.cu = cu;
+		this.filePath = filePath;
+		this.writer = writer;
+	}
+
+    @Override
+    public boolean visit(MethodInvocation node) {
+        if (node.getName().getIdentifier().equals("getCause")) {
+            	node.resolveMethodBinding();
+            	Expression expr = node.getExpression();
+            	String className= expr.resolveTypeBinding()!=null ?(expr.resolveTypeBinding().getQualifiedName()):null;
+            	Class<?> clazz=null;
+				if (className != null) {
+					try {
+						clazz = Class.forName(className);
+					} catch (ClassNotFoundException e) {
+						// No action
+					}
+				}
+            	if(clazz != null && Throwable.class.isAssignableFrom(clazz));
+            	{
+	            	int lineNumber = cu.getLineNumber(node.getStartPosition());
+	            	try {
+						writer.write(filePath + "," + lineNumber + "," + "getCause" + "\n");
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}
+					//System.out.println("Found getCause call on throwable at line " + lineNumber + " of " + filePath);
+            	}
+            	
+            
+        }
+        return super.visit(node);
+    }
+}
--- Assignment/Antipattern_Detector/src/main/java/com/concordia/soen/sma/ProjectReader.java
+++ Assignment/Antipattern_Detector/src/main/java/com/concordia/soen/sma/ProjectReader.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.util.Collection;
 
 import org.apache.commons.io.FileUtils;
@@ -15,6 +16,7 @@ public class ProjectReader {
 	// The main method of the class which reads a java file input directory and detects anti-patterns in each file.
 	public static void main(String[] args) throws IOException {
 		String directoryPath = "D:\\SoftwareMiningandAnalysis\\hibernate-orm";
+		//String directoryPath="C:\\Users\\editi\\Desktop\\SOEN6591\\hibernate-orm-main";
 		Collection<File> files = FileUtils.listFiles(new File(directoryPath), new String[] { "java" }, true);
 
 		// Create a FileWriter object to write to a file
@@ -26,6 +28,8 @@ public class ProjectReader {
 			try {
 				// Pass the FileWriter object to the NestedTryBlockDetector instance
 				detectExceptionalHandlingAntiPatterns(filePath, csvWriter);
+				//Pass  FileWriter object to the GetCauseDetector
+				detectExceptionalHandlingAntiPatternGetCause(file,directoryPath,filePath, csvWriter);
 			} catch (IOException e) {
 				System.err.println("Error reading file " + filePath + ": " + e.getMessage());
 			}
@@ -33,6 +37,28 @@ public class ProjectReader {
 
 		// Close the FileWriter object after processing all files
 		csvWriter.close();
+	}
+
+	private static void detectExceptionalHandlingAntiPatternGetCause(File file, String directoryPath, String filePath,
+			FileWriter csvWriter) throws IOException {
+		byte[] fileBytes = Files.readAllBytes(file.toPath());
+        String fileContents = new String(fileBytes, Charset.defaultCharset());
+        // Creates a new parser instance for the specified API level.
+        ASTParser parser = ASTParser.newParser(AST.JLS19);
+        // Parses the source code and returns the root AST node.
+        parser.setSource(fileContents.toCharArray());
+        parser.setKind(ASTParser.K_COMPILATION_UNIT);
+        
+        
+        String[] classpathEntries = System.getProperty("java.class.path").split(File.pathSeparator);
+        
+        parser.setEnvironment(classpathEntries, new String[] { directoryPath }, new String[] { "UTF-8" }, true);
+        parser.setResolveBindings(true);
+        parser.setBindingsRecovery(true);
+        parser.setUnitName(directoryPath);
+        CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+        cu.accept(new GetCauseDetector(cu,filePath,csvWriter));
+		
 	}
 
 	private static void detectExceptionalHandlingAntiPatterns(String filePath, FileWriter writer) throws IOException {
@@ -44,5 +70,7 @@ public class ProjectReader {
 		// Pass the FileWriter object to the NestedTryBlockDetector instance
 		cu.accept(new NestedTryBlockDetector(cu, filePath, writer));
 	}
+	
+	
 
 }
