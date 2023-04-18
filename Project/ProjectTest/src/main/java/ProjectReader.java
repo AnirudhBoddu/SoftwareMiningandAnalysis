908be697f6ec708bf145e9aa8d7131a936d331c0
Anirudh Boddu <anirudh.boddu97@gmail.com>
2023-04-18 11:25:05 -0400

    Updating Project Reader class
    This commit modified project reader class to write filenames and respective antipattern counts to a csv file. This is useful for further data analysis.

--- Project/ProjectTest/src/main/java/ProjectReader.java
+++ Project/ProjectTest/src/main/java/ProjectReader.java
@@ -1,5 +1,7 @@
 // This class is used to read a java project directory and detect exceptional handling anti-patterns in the code.
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
@@ -7,41 +9,45 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
 
 import java.io.File;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.sql.SQLOutput;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 public class ProjectReader {
     private static final String[] FILE_EXTENSIONS = new String[] { "java" };
     static Map<String,Integer> hm= new HashMap<>();
 
     public static void main(String[] args) throws IOException {
-        String directoryPath = "C:\\Users\\ss161\\Downloads\\hibernate-orm-5.2.0\\hibernate-orm-5.2.0";
+        String directoryPath = "C:\\Users\\ANIRUDH BODDU\\Desktop\\Software Mining and Analysis\\hibernate-orm-5.2.0\\hibernate-orm-5.2.0";
         Collection<File> files = FileUtils.listFiles(new File(directoryPath), FILE_EXTENSIONS, true);
+        List<String[]> output = new ArrayList<>();
 
         for (File file : files) {
             String filePath = file.getAbsolutePath();
             try {
+                int countCatchAndReturnNull, countOverCatch, countCatchAndDoNothing;
                 CompilationUnit cu = parseFile(filePath);
                 cu.accept(new CatchAndReturnNullDetector(cu, file));
+                cu.accept(new OverCatchDetector(cu, file));
+                cu.accept(new CatchAndDoNothingDetector(cu, file));
+                countCatchAndReturnNull = CatchAndReturnNullDetector.CatchAndReturnNullCount();
+                countOverCatch = OverCatchDetector.OverCatchCount();
+                countCatchAndDoNothing = CatchAndDoNothingDetector.CatchAndDoNothingCount();
+                output.add(new String[] {file.getName(), String.valueOf(countCatchAndReturnNull), String.valueOf(countOverCatch), String.valueOf(countCatchAndDoNothing)});
             } catch (IOException e) {
                 System.err.println("Error reading file " + filePath + ": " + e.getMessage());
             }
-            hm.put(file.getName(), CatchAndReturnNullDetector.CatchAndReturnNullCount());
         }
 
-        Iterator hmIterator= hm.entrySet().iterator();
-
-        while(hmIterator.hasNext()){
-            Map.Entry m = (Map.Entry)hmIterator.next();
-            System.out.println(m.getKey()+" "+m.getValue());
+        try (CSVPrinter printer = new CSVPrinter(new FileWriter("antipatternsCount.csv"), CSVFormat.DEFAULT)) {
+            printer.printRecord("Filename", "countCatchAndReturnNull", "countOverCatch", "countCatchAndDoNothing");
+            for (String[] row : output) {
+                printer.printRecord(row);
+            }
         }
-
     }
+
 
     private static CompilationUnit parseFile(String filePath) throws IOException {
         String source = FileUtils.readFileToString(new File(filePath), Charset.defaultCharset());
@@ -53,5 +59,4 @@ public class ProjectReader {
         parser.setUnitName(filePath);
         return (CompilationUnit) parser.createAST(null);
     }
-
 }
