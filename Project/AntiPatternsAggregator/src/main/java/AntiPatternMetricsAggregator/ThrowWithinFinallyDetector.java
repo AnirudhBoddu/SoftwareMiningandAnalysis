68f5199be260d0f6cf8330d30c988f5cefec9340
AditiAditiCU <a_aditi@live.concordia.ca>
2023-04-28 07:15:27 -0400

    Changes to throwWithinFinallyDetector Code

--- Project/AntiPatternsAggregator/src/main/java/AntiPatternMetricsAggregator/ThrowWithinFinallyDetector.java
+++ Project/AntiPatternsAggregator/src/main/java/AntiPatternMetricsAggregator/ThrowWithinFinallyDetector.java
@@ -8,50 +8,47 @@ import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.TryStatement;
 
 public class ThrowWithinFinallyDetector extends ASTVisitor {
-	 private CompilationUnit cu;
-	    private File file;
-	    private static int throwWithinFinallyCount;
-	    static File temp= new File("C:\\Users\\ss161\\Downloads\\apache-ant-1.9.7-src\\example.java");
+	private CompilationUnit cu;
+	private File file;
+	private static int throwWithinFinallyCount;
+	static File temp = new File("C:\\Users\\ss161\\Downloads\\apache-ant-1.9.7-src\\example.java");
 
-	    public ThrowWithinFinallyDetector(CompilationUnit cu, File file) {
-	        this.cu = cu;
-	        this.file = file;
-	    }
-    
-   
-    @Override
-    public boolean visit(TryStatement node) {
-    	 if(!temp.getName().equals(file.getName())){
-    		 throwWithinFinallyCount= 0;
-         }
-    	for (Object obj : node.getBody().statements()) {
-            if (obj instanceof Statement) {
-                Statement statement = (Statement) obj;
-                statement.accept(this);
-            }
-        }
+	public ThrowWithinFinallyDetector(CompilationUnit cu, File file) {
+		this.cu = cu;
+		this.file = file;
+	}
 
-        // Visit the statements in the finally block (if any)
-        if (node.getFinally() != null) {
-        	for (Object obj : node.getFinally().statements()) {
-                if (obj instanceof TryStatement) {
-                    TryStatement tryStatement = (TryStatement) obj;
-                    // Check if the try-catch block contains a throw statement
-                    if (tryStatement.getBody().statements().stream().anyMatch(s -> s.toString().contains("throw"))) {
-                    	throwWithinFinallyCount++;
-                       // hasThrownInFinallyBlock = true;
-                        break;
-                    }
-                }
-            }
-        }
-        temp= file;
+	@Override
+	public boolean visit(TryStatement node) {
+		
+		if (!temp.getName().equals(file.getName())) {
+			throwWithinFinallyCount = 0;
+		}
+		/*
+		 * for (Object obj : node.getBody().statements()) { if (obj instanceof
+		 * Statement) { Statement statement = (Statement) obj; statement.accept(this); }
+		 * }
+		 */
+
+		// Visit the statements in the finally block (if any)
+		if (node.getFinally() != null) {
+			for (Object obj : node.getFinally().statements()) {
+				if (obj instanceof TryStatement) {
+					TryStatement tryStatement = (TryStatement) obj;
+					// Check if the try-catch block contains a throw statement
+					if (tryStatement.getBody().statements().stream().anyMatch(s -> s.toString().contains("throw"))) {
+						throwWithinFinallyCount++;
+						break;
+					}
+				}
+			}
+		}
+		temp = file;
 		return super.visit(node);
-    	
-    }
-    
-    public static int ThrowWithinFinallyDetectorCount() {
-        return throwWithinFinallyCount;
-    }
-}
 
+	}
+
+	public static int ThrowWithinFinallyDetectorCount() {
+		return throwWithinFinallyCount;
+	}
+}
