1836511e27056e325fc6ea8c297323af5929ef59
AditiAditiCU <a_aditi@live.concordia.ca>
2023-04-30 20:44:36 -0400

    changes

--- Project/AntiPatternsAggregator/src/main/java/AntiPatternMetricsAggregator/ThrowWithinFinallyDetector.java
+++ Project/AntiPatternsAggregator/src/main/java/AntiPatternMetricsAggregator/ThrowWithinFinallyDetector.java
@@ -4,7 +4,6 @@ import java.io.File;
 
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.TryStatement;
 
 public class ThrowWithinFinallyDetector extends ASTVisitor {
@@ -20,6 +19,7 @@ public class ThrowWithinFinallyDetector extends ASTVisitor {
 
 	@Override
 	public boolean visit(TryStatement node) {
+		//check if file in temporary equals file 
 		if (!temp.getName().equals(file.getName())) {
 			throwWithinFinallyCount = 0;
 		}
